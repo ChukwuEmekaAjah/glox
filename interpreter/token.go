4ee9242eb056ec1fc1acc7663203f7cb6630188b
ChukwuEmekaAjah <talk2ajah@gmail.com>
2023-11-27 08:44:32 +0000

    Add keywords map

--- interpreter/token.go
+++ interpreter/token.go
@@ -39,3 +39,22 @@ func NewToken(tokenType TokenType, lexeme string, literal interface{}, line uint
 func (t Token) String() string {
 	return fmt.Sprintf("%s %s [%v]", t.tokenType, t.literal, t.line)
 }
+
+var Keywords map[string]TokenType = map[string]TokenType{
+	"and":    "AND",
+	"class":  "CLASS",
+	"else":   "ELSE",
+	"false":  "FALSE",
+	"for":    "FOR",
+	"fun":    "FUN",
+	"if":     "IF",
+	"nil":    "NIL",
+	"or":     "OR",
+	"print":  "PRINT",
+	"return": "RETURN",
+	"super":  "SUPER",
+	"this":   "THIS",
+	"true":   "TRUE",
+	"var":    "VAR",
+	"while":  "WHILE",
+}
